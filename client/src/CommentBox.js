6996b0a2601209dc01e1351b837c3ad0041afa40
Joe Kovach <joekovach82@gmail.com>
2018-08-18 15:08:31 -0700

    Finishing The MERN Comment Box

--- client/src/CommentBox.js
+++ client/src/CommentBox.js
@@ -39,7 +39,87 @@ class CommentBox extends Component {
       });
   }
 
+  onChangeText = (e) => {
+    const newState = { ...this.state };
+    newState[e.target.name] = e.target.value;
+    this.setState(newState);
+  }
+
+  onUpdateComment = (id) => {
+    const oldComment = this.state.data.find(c => c._id === id);
+    if (!oldComment) return;
+    this.setState({
+        author: oldComment.author,
+        text: oldComment.text,
+        updateId: id
+    });
+  }
+
+  onDeleteComment = (id) => {
+    const i = this.state.data.findIndex(c => c._id === id);
+    const data = [
+      ...this.state.data.slice(0, i),
+      ...this.state.data.slice(i + 1),
+    ];
+    this.setState({ data });
+    fetch(`api/comments/${id}`, { method: 'DELETE' })
+      .then(res => res.json()).then((res) => {
+        if (!res.success) this.setState({ error: res.error });
+      });
+  }
+
+  submitComment = (e) => {
+    e.preventDefault();
+    const { author, text, updateId } = this.state;
+    if (!author || !text) return;
+    if (updateId) {
+      this.submitUpdatedComment();
+    } else {
+      this.submitNewComment();
+    }
+  }
+
+  submitNewComment = () => {
+    const { author, text } = this.state;
+    const data = [
+      ...this.state.data,
+      {
+        author,
+          text,
+          _id: Date.now().toString(),
+          updatedAt: new Date(),
+          createdAt: new Date()
+      },
+    ];
+    this.setState({ data });
+    fetch('/api/comments', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ author, text }),
+    }).then(res => res.json()).then((res) => {
+      if (!res.success) this.setState({ error: res.error.message || res.error });
+      else this.setState({ author: '', text: '', error: null });
+    });
+  }
+
+  submitUpdatedComment = () => {
+    const { author, text, updateId } = this.state;
+    fetch(`/api/comments/${updateId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ author, text }),
+    }).then(res => res.json()).then((res) => {
+      if (!res.success) this.setState({ error: res.error.message || res.error });
+      else this.setState({ author: '', text: '', updateId: null });
+    });
+  }
+
   render() {
+    <CommentList
+      data={this.state.data}
+      handleDeleteComment={this.onDeleteComment}
+      handleUpdateComment={this.onUpdateComment}
+    />
     return (
       <div className="container">
         <div className="comments">
